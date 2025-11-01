612e568f2101ca79750f576d87b1f43e0d61aba6
zhuzhifang <31278216+byxiaozhi@users.noreply.github.com>
2025-11-01 21:59:16 +0800

    Add initial test project and Class1 implementation
    Introduced DouyinLiveCapture.Tests project with MSTest setup and a sample test for Class1. Added Class1 to the main project with a Hello method. Updated solution and project files to include the new test project and enable preview language features.

---
+++ src/DouyinLiveCapture.Tests/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace DouyinLiveCapture.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new Class1().Hello();
+        }
+    }
+}
---
+++ src/DouyinLiveCapture/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DouyinLiveCapture
+{
+    public class Class1
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello from Class1 in DouyinLiveCapture!");
+        }
+    }
+}
